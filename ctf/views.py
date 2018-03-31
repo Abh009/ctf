87d68f41820863ac2dc16cde0edf690d17505dd6
Sidharth V <sidharthv96@gmail.com>
2018-04-01 00:06:37 +0530

    Login Required

--- ctf/views.py
+++ ctf/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404, redirect
-from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.csrf import csrf_exempt,login_required
 from ctf.models import Problems
 from django.contrib.auth import logout
 # Create your views here.
@@ -35,6 +35,7 @@ def home(request):
     return render(request,'index.html',{})
 
 @csrf_exempt
+@login_required
 def submit(request):
     if request.is_ajax():
        if request.method == 'POST':
