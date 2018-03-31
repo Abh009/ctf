39b785481eac165dcf6e9bcaad802b0ccfec7959
Abhinav KM <abhinavkm184@gmail.com>
2018-03-31 21:03:06 +0530

    login page added

--- hackprix/urls.py
+++ hackprix/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('problems/',views.problems),
     path('submit/',views.submit),
     path('play/',views.terminal),
+    path('login/',views.login),
     path('',views.home),
 ]
 
