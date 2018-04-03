98a43ade9a70fecb23286658ed2757118db186c1
Abhinav KM <abhinavkm184@gmail.com>
2018-04-03 14:00:12 +0530

    added user ban and attempt logging

--- ctf/models.py
+++ ctf/models.py
@@ -29,6 +29,7 @@ class BannedUser(models.Model):
     is_banned = models.BooleanField(default=False)
 
 class AttemptLog(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE,primary_key=False)
+    user = models.IntegerField(null=False,blank=False)
+    attempt_quest = models.OneToOneField(Problems,on_delete=models.CASCADE)
     attempt_text = models.CharField(max_length=1000,blank=False, null=False)
     time = models.DateTimeField(auto_now=False,auto_now_add=False)
