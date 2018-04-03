a44ffb21170b309e50d78e64dd456e4c59f8e8f2
Abhinav KM <abhinavkm184@gmail.com>
2018-04-03 21:30:23 +0530

    attempt log in progress

--- ctf/models.py
+++ ctf/models.py
@@ -33,7 +33,7 @@ class BannedUser(models.Model):
 class UserLog(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     attempt_quest = models.OneToOneField(Problems,on_delete=models.CASCADE)
-    attempt_text = models.CharField(blank=False, null=False)
+    attempt_text = models.CharField(max_length=1000,blank=False, null=False)
     time = models.DateTimeField(auto_now=False,auto_now_add=False)
 
     def __str__(self):
